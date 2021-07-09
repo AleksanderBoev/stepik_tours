abb0250f449f17ae671d8cc7b9b5afca1e84af83
AleksanderBoev <boev-a-i@yandex.ru>
2021-07-09 21:45:43 +0300

    3

--- tours/views.py
+++ tours/views.py
@@ -4,16 +4,34 @@ import stepic_tours.data as data
 
 # Create your views here.
 def main_view(request):
+
     return render(request, 'index.html')
 
 
 def departure_view(request, departure):
     tours = dict()
+    min_nights = 0
+    max_nights = 0
+    min_price = 0
+    max_price = 0
     for key, inf in data.tours.items():
         if inf['departure'] == departure:
             tours[key] = inf
-    print(tours)
-    return render(request, 'departure.html', {'tours': tours})
+            if (inf['price'] < min_price) or min_price == 0:
+                min_price = inf['price']
+            if inf['price'] > max_price:
+                max_price = inf['price']
+            if inf['nights'] < min_nights or min_nights == 0:
+                min_nights = inf['nights']
+            if inf['nights'] > max_nights:
+                max_nights = inf['nights']
+    return render(request, 'departure.html', {'tours': tours,
+                                              'count': len(tours),
+                                              'min_nights': min_nights,
+                                              'max_nights': max_nights,
+                                              'min_price': min_price,
+                                              'max_price': max_price,
+                                              })
 
 
 def tour_view(request, idtour=None):
